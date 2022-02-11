50783f7acd7504d2cc8743e8854cc915beed3fb5
Kristjan <krix@planet.ee>
2022-02-11 19:21:26 +0200

    docker-compose file and Dockerfile complete

--- src/test/java/com/example/omblusapi/OmblusapiApplicationTests.java
+++ src/test/java/com/example/omblusapi/OmblusapiApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.omblusapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = OmblusapiApplicationTests.class)
 class OmblusapiApplicationTests {
 
 	@Test
